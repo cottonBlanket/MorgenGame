fe224794d2b82f1ee2ec8186128795d84c6b2a3f
Danil <102159807+cottonBlanket@users.noreply.github.com>
2022-06-06 00:33:53 +0500

    Написал автодокументацию

--- MorgenGame/View/Home.cs
+++ MorgenGame/View/Home.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,13 +8,33 @@ namespace MorgenGame
 {
     class Home : IGameObject
     {
+        /// <summary>
+        /// позиция по абсцисее
+        /// </summary>
         public int posX { get; set; }
+        /// <summary>
+        /// позиция по ординате
+        /// </summary>
         public int posY { get; set; }
-
+        /// <summary>
+        /// размеры по абсциссе
+        /// </summary>
         public int sizeX { get; set; }
+        /// <summary>
+        /// размеры по ординате
+        /// </summary>
         public int sizeY { get; set; }
+        /// <summary>
+        /// картинка объекта
+        /// </summary>
         public Image picture { get; set; }
 
+        /// <summary>
+        /// конструктор класса
+        /// для инициализации игрока с заданными значениями координат
+        /// </summary>
+        /// <param name="pX">начальная позиция по абсциссе</param>
+        /// <param name="pY">начальная позиция по ординате</param>
         public Home(int pX, int pY)
         {
             posX = pX;
