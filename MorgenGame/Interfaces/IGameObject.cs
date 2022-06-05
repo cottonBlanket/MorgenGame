f98fadd96c27d5eba9afbf6cb8b07ea08521a30c
Danil <102159807+cottonBlanket@users.noreply.github.com>
2022-06-06 00:32:03 +0500

    Написал автодокументацию

--- MorgenGame/Interfaces/IGameObject.cs
+++ MorgenGame/Interfaces/IGameObject.cs
@@ -1,15 +1,31 @@
-﻿using System.Drawing;
+using System.Drawing;
 
 namespace MorgenGame
 {
+    /// <summary>
+    /// интерфейс игрового объекта
+    /// </summary>
     internal interface IGameObject
     {
+        /// <summary>
+        /// позиция по абсцисее
+        /// </summary>
         int posX { get; set; }
+        /// <summary>
+        /// позиция по ординате
+        /// </summary>
         int posY { get; set; }
-
+        /// <summary>
+        /// размеры по абсциссе
+        /// </summary>
         int sizeX { get; set; }
+        /// <summary>
+        /// размеры по ординате
+        /// </summary>
         int sizeY { get; set; }
-
+        /// <summary>
+        /// картинка объекта
+        /// </summary>
         Image picture { get; set; }
     }
 }
