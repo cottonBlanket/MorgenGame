35b3cc058be89a398af739038843b9cf4ecea107
Danil <102159807+cottonBlanket@users.noreply.github.com>
2022-06-06 00:32:30 +0500

    Написал автодокументацию

--- MorgenGame/Model/GameState.cs
+++ MorgenGame/Model/GameState.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,6 +6,9 @@ using System.Threading.Tasks;
 
 namespace MorgenGame
 {
+    /// <summary>
+    /// определяет статус игры
+    /// </summary>
     enum GameState
     {
         MainMenu,
